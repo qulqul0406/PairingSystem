c9c8697dd908a48797d6ec9c8ef14afeaddf38b2
sora.huang <sora.huang@nec.com.tw>
2024-02-04 15:49:11 +0800

    後端-添加WebConfig

--- back/pairing-system/src/main/java/com/example/pairingsystem/PairingSystemApplication.java
+++ back/pairing-system/src/main/java/com/example/pairingsystem/PairingSystemApplication.java
@@ -2,10 +2,23 @@ package com.example.pairingsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
 @SpringBootApplication
 public class PairingSystemApplication {
 
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**").allowedOrigins("http://localhost:3000") // 允許訪問的前端地址
+                    .allowedMethods("GET", "POST", "PUT", "DELETE") // 允許的HTTP方法
+                    .allowCredentials(true); // 允許攜帶cookie等認證信息
+        }
+    }
     public static void main(String[] args) {
         SpringApplication.run(PairingSystemApplication.class, args);
     }
